1af32997b9e1984a2f6f3fbe17d454c6e386799d
brian <bmarsha72@gmail.com>
2016-05-16 17:12:39 -0500

    audio javascript alterations to runPlayer

--- js/init.js
+++ js/init.js
@@ -1,7 +1,15 @@
 (function($){
+
   $(function(){
 
+    $('#statementlabel').click(function(){
+        this.value = '';
+    })
+
+
+
     $('.button-collapse').sideNav();
+
 
   }); // end of document ready
 })(jQuery); // end of jQuery name space
--- script/scripts.js
+++ script/scripts.js
@@ -1,130 +1,117 @@
 
 $(document).ready(function(){
 
+
+
   var clipsSelected = {}
   //object that holds the selected clips
 
+  //things to do when page loads:
 
-//selected clips will go inside the onload
+
   $('#statement').change(function(){
     console.log('statement is working')
     clipsSelected.statement = $(this, ':selected').val();
   });//end of #statement onchange push to clipsSelected object
+
   $('#noun1').change(function(){
     console.log('noun1 is working')
     clipsSelected.noun1 = $(this, ':selected').val();
   });//end of #noun1 onchange push to clipsSelected object
+
   $('#noun2').change(function(){
     console.log('noun2 is working')
     clipsSelected.noun2 = $(this, ':selected').val();
   });//end of #noun2 onchange push to clipsSelected object
+
   $('#person').change(function(){
     console.log('person is working')
     clipsSelected.person = $(this, ':selected').val();
   });//end of #person onchange push to clipsSelected object
+
   $('#noun3').change(function(){
     console.log('noun3 is working')
     clipsSelected.noun3 = $(this, ':selected').val();
   });//end of #noun3 onchange push to clipsSelected object
 
 
-  //button to launch the player and checkLength function:
-  $('#execute').click(function(){
-    $('#execute').attr('disabled','true');
-    checkLength(clipsSelected);
-    $('#execute').removeAttr('disabled');
-  });
-
 
 
 //to check to make sure there are four (or more?)
 //selection if done out of order:
-function checkLength(obj){
-  var counter = 0
-  for (var prop in obj){
-    counter++
-    if(counter === 4){
-      console.log(counter)
-      runPlayer(clipsSelected)
-    }
-    else{
-      console.log("please make all selections")
-      return
-    }
-    console.log("obj: " + prop + " = " + obj[prop])
-  }
-}//end of checkLength function
+// function checkLength(obj){
+//   var counter = 0
+//   for (var prop in obj){
+//     counter++
+//     if(counter === 5){
+//       console.log(counter)
+//       runPlayer(clipsSelected)
+//     }
+//     else{
+//       console.log("please make all selections")
+//       return
+//     }
+//     console.log("obj: " + prop + " = " + obj[prop])
+//   }
+// }//end of checkLength function
 
 //assemble the runPlayer once the play order is determined.
-function runPlayer(){
-
-  var one       = 'audio/HitMeHardWithTheWomenCardThatsEnough.mp3'
-  var two       = 'audio/Igohomeandmywifesaid.mp3'
-  var three     = 'audio/WAT.mp3'
-  var four      = 'audio/nobodyrespectswomentmorethandonaldtrump.mp3'
-  var five      = 'audio/soweregoingtostraightenout.mp3'
-  var six       = 'audio/theylovemeIlovethem.mp3'
-  var seven     = 'audio/WAT.mp3'
-  var eight     = 'audio/IvyLeagueBetterWordThan.mp3'
-  var nine      = 'audio/ImeanIlookatthisguy.mp3'
-  var ten       = 'audio/hesalowenergy...weneedlotsof.mp3'
-  var eleven    = 'audio/WAT.mp3'
-  var twelve    = 'audio/AnchorBaby.mp3'
-  var thirteen  = 'audio/itstoolong..whowantstowatchfiftyhoursofthisstuffright.mp3'
+function runPlayer(randomName){
 
   var audio1 = document.createElement('audio');
-  audio1.src = one;
+  audio1.src = 'audio/HitMeHardWithTheWomenCardThatsEnough.mp3';
   audio1.addEventListener('ended', function(){
   var audio2 = document.createElement('audio');
-  audio2.src = two;
+  audio2.src = 'audio/Igohomeandmywifesaid.mp3';
   audio2.addEventListener('ended', function(){
   var audio3 = document.createElement('audio');
-  audio3.src = three;
+  audio3.src = randomName.statement;
   audio3.addEventListener('ended', function(){
   var audio4 = document.createElement('audio');
-  audio4.src = four;
+  audio4.src = 'audio/WAT.mp3';
   audio4.addEventListener('ended', function(){
   var audio5 = document.createElement('audio');
-  audio5.src = five;
+  audio5.src = 'audio/nobodyrespectswomentmorethandonaldtrump.mp3';
   audio5.addEventListener('ended', function(){
   var audio6 = document.createElement('audio');
-  audio6.src = six;
+  audio6.src = 'audio/soweregoingtostraightenout.mp3';
   audio6.addEventListener('ended', function(){
   var audio7 = document.createElement('audio');
-  audio7.src = seven;
+  audio7.src = randomName.noun1;
   audio7.addEventListener('ended', function(){
   var audio8 = document.createElement('audio');
-  audio8.src = eight;
+  audio8.src = 'audio/theylovemeIlovethem.mp3';
   audio8.addEventListener('ended', function(){
   var audio9 = document.createElement('audio');
-  audio9.src = nine;
+  audio9.src = 'audio/WAT.mp3';
   audio9.addEventListener('ended', function(){
   var audio10 = document.createElement('audio');
-  audio10.src = ten;
+  audio10.src = 'audio/IvyLeagueBetterWordThan.mp3';
   audio10.addEventListener('ended', function(){
   var audio11 = document.createElement('audio');
-  audio11.src = eleven;
+  audio11.src = randomName.noun2;
   audio11.addEventListener('ended', function(){
   var audio12 = document.createElement('audio');
-  audio12.src = twelve;
+  audio12.src = 'audio/ImeanIlookatthisguy.mp3';
   audio12.addEventListener('ended', function(){
   var audio13 = document.createElement('audio');
-  audio13.src = thirteen;
+  audio13.src = randomName.person;
   audio13.addEventListener('ended', function(){
   var audio14 = document.createElement('audio');
-  audio14.src = fourteen;
+  audio14.src = 'audio/hesalowenergy...weneedlotsof.mp3';
   audio14.addEventListener('ended', function(){
   var audio15 = document.createElement('audio');
-  audio15.src = fifteen;
+  audio15.src = randomName.noun3;
   audio15.addEventListener('ended', function(){
   var audio16 = document.createElement('audio');
-  audio16.src = sixteen;
+  audio16.src = 'audio/WAT.mp3';
   audio16.addEventListener('ended', function(){
   var audio17 = document.createElement('audio');
-  audio17.src = seventeen;
+  audio17.src = 'audio/AnchorBaby.mp3';
   audio17.addEventListener('ended', function(){
   var audio18 = document.createElement('audio');
-  audio18.src = eighteen;
+  audio18.src = 'audio/itstoolong..whowantstowatchfiftyhoursofthisstuffright.mp3';
+  audio18.play()
 
   })//end audio17 event listener
   audio17.play()
@@ -161,5 +148,22 @@ function runPlayer(){
   });//end of audio1 event listener
   audio1.play();//plays the first clip
 
-}//end of runPlayer
+};//end of runPlayer
+
+
+  //button to launch the player and checkLength function:
+  $('#execute').click(function(){
+    //$('#execute').attr('disabled','true');
+    runPlayer();
+      //$('#execute').removeAttr('disabled');
+  });
+
+function fakePlayer(){
+  var audio88 = document.createElement('audio');
+  audio88.src = twelve
+  audio88.play()
+  console.log("thisworks")
+}
+
+
 });//end of document ready function
